e1bf6f4fdc63fb6f44ed8461c4d0fcc189928db1
beskep <beskep@naver.com>
2025-07-24 14:48:35 +0900

    refactor: remove unnecessary future annotations

--- eco2auto/__init__.py
+++ eco2auto/__init__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from . import app, automate, report, utils
 
 __all__ = ['app', 'automate', 'report', 'utils']
--- eco2auto/__main__.py
+++ eco2auto/__main__.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from eco2auto.app import app
 
 if __name__ == '__main__':
